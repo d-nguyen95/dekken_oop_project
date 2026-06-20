90651e4bd841bb8400ca843e666f6a5e16377bf5
Damien Nguyen <damien.nguyen95@gmail.com>
2026-06-20 15:35:43 +0100

    add move database

---
+++ movelist.py
@@ -0,0 +1,37 @@
+devil_jin = {
+    'electric_wind_god_fist': {'name': 'Ewgf', 'hitbox': 'high', 'damage': 23, 'startup frames': 13, 'on block': 5, 'on hit': 39},
+
+    {'name': '3,1', 'hitbox': ['mid', 'high'], 'damage': [16, 11], 'startup frames': 17, 'on block': 2, 'on hit': 8},
+    
+    {'name': 'ws1,1', 'hitbox': ['mid', 'high'], 'damage': [12, 10], 'startup frames': 13, 'on block': 11, 'on hit': 16},
+    
+    {'name': 'ws2', 'hitbox': 'mid', 'damage': 20, 'startup frames': 14, 'on block': -12, 'on hit': 59},
+
+    {'name': 'hellsweep', 'hitbox': 'low', 'damage': 25, 'startup frames': 16, 'on block': -23, 'on hit': 42},
+}
+
+
+zafina = {
+    {'name': 'df2', 'hitbox': 'mid', 'damage': 23, 'startup frames': 16, 'on block': -12, 'on hit': 32},
+
+    {'name': 'snake edge', 'hitbox': 'low', 'damage': 45, 'startup frames': 28, 'on block': -23, 'on hit': 54},
+    
+    {'name': 'ws1,1', 'hitbox': ['mid', 'high'], 'damage': [12, 10], 'startup frames': 13, 'on block': 11, 'on hit': 16},
+    
+    {'name': 'ws2', 'hitbox': 'mid', 'damage': 20, 'startup frames': 14, 'on block': -12, 'on hit': 59},
+
+    {'name': 'hellsweep', 'hitbox': 'low', 'damage': 25, 'startup frames': 16, 'on block': -23, 'on hit': 42},
+}
+
+
+generic_moves = {
+    {'name': 'dickjab', 'hitbox': 'low', 'damage': 5, 'startup frames': 10, 'on block': -5, 'on hit': 6},
+
+    {'name': 'jab', 'hitbox': 'high', 'damage': 5, 'startup frames': 10, 'on block': 1, 'on hit': 8},
+    
+    {'name': 'grab', 'hitbox': 'high', 'damage': 40, 'startup frames': 13, 'on block': 0, 'on hit': 5},
+    
+    {'name': 'block', 'hitbox': None, 'damage': None, 'startup frames': None, 'on block': None, 'on hit': None},
+}
+
+print('')
